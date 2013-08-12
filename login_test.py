5be33bfd3b13ff900b87ba08fce0924e6557610b
ifreefly <idevcod@gmail.com>
2013-08-12 17:19:43 +0800

    百度爬虫参考
    re文件新增参考链接

--- login_test.py
+++ login_test.py
@@ -4,16 +4,16 @@ import re;
 #------------------------------------------------------------------------------
 
 def main():
-    userMainUrl = "http://www.songtaste.com/user/351979/";
+    userMainUrl = "http://tieba.baidu.com/p/2525590943";
     req = urllib2.Request(userMainUrl);
     resp = urllib2.urlopen(req);
     respHtml = resp.read();
     #print "respHtml=",respHtml; # you should see the ouput html
     #<h1 class="h1user">crifan</h1>
-    foundH1user = re.search('<h1\s+?class="h1user">(?P<h1user>.+?)</h1>', respHtml);
+    foundH1user = re.search('<h1\s+class="(.*?)"\s+title="(.*?)"\s+>(.*?)<\/h1>', respHtml);
     print "foundH1user=",foundH1user;
     if(foundH1user):
-        h1user = foundH1user.group("h1user");
+        h1user = foundH1user.group("title");
         print "h1user=",h1user; 
 ###############################################################################
 if __name__=="__main__":
