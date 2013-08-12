ba28ecdebc508c35f116bb1d7ec9f76c746cf2e4
ifreefly <idevcod@gmail.com>
2013-08-12 16:43:49 +0800

    nothing

--- login_test.py
+++ login_test.py
@@ -1,10 +1,20 @@
+#---------------------------------import---------------------------------------
 import urllib2;
+import re;
+#------------------------------------------------------------------------------
 
 def main():
-	userMailUrl="http://tieba.baidu.com/f?kw=%BB%FD%CA%AF%CC%C3&frs=yqtb"
-	req=urllib2.Request(userMailUrl)
-	resp=urllib2.urlopen(req)
-	respHtml=resp.read()
-	print "respHtml",respHtml
-
-main()
+    userMainUrl = "http://www.songtaste.com/user/351979/";
+    req = urllib2.Request(userMainUrl);
+    resp = urllib2.urlopen(req);
+    respHtml = resp.read();
+    #print "respHtml=",respHtml; # you should see the ouput html
+    #<h1 class="h1user">crifan</h1>
+    foundH1user = re.search('<h1\s+?class="h1user">(?P<h1user>.+?)</h1>', respHtml);
+    print "foundH1user=",foundH1user;
+    if(foundH1user):
+        h1user = foundH1user.group("h1user");
+        print "h1user=",h1user; 
+###############################################################################
+if __name__=="__main__":
+    main();
