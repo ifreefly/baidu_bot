fd8833f83ee89111aad6b4edb42fb4b73e7f2a63
ifreefly <idevcod@gmail.com>
2013-08-12 17:43:25 +0800

    自己的爬虫能用了
    虽然很慢

--- login_test.py
+++ login_test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #---------------------------------import---------------------------------------
 import urllib2;
 import re;
@@ -10,11 +11,11 @@ def main():
     respHtml = resp.read();
     #print "respHtml=",respHtml; # you should see the ouput html
     #<h1 class="h1user">crifan</h1>
-    foundH1user = re.search('<h1\s+class="(.*?)"\s+title="(.*?)"\s+>(.*?)<\/h1>', respHtml);
+    foundH1user = re.search(r'<h1.*?>(.*?)</h1>', respHtml);
     print "foundH1user=",foundH1user;
     if(foundH1user):
-        h1user = foundH1user.group("title");
-        print "h1user=",h1user; 
+        h1user = foundH1user.group(1);
+        print "文章标题:",h1user; 
 ###############################################################################
 if __name__=="__main__":
     main();
